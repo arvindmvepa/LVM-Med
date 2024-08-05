03d5048eee41b552f0ff1c1a035322989413762a
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-08-04 22:15:55 -0700

    bug

--- segmentation_models_pytorch_example/encoders/resnet.py
+++ segmentation_models_pytorch_example/encoders/resnet.py
@@ -65,7 +65,7 @@ class ResNetEncoder(ResNet, EncoderMixin):
         return features
 
     def load_state_dict(self, state_dict, **kwargs):
-        print("state_dict: ", state_dict.keys())
+        print("state_dict: ", state_dict)
         state_dict.pop("fc.bias", None)
         state_dict.pop("fc.weight", None)
         super().load_state_dict(state_dict, **kwargs)
